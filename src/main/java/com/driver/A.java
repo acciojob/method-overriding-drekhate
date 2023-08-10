fa124d57060d3a6b40e23749ce042af832a7f226
Dipak Rekhate <drekhate@gmail.com>
2023-08-10 15:13:34 +0530

    assi done on overriding

---
+++ src/main/java/com/driver/A.java
@@ -0,0 +1,10 @@
+package com.driver;
+
+public class A {
+    public String meth() {
+        String s = "Invoking method from class A";
+        return s;
+    }
+
+
+}
---
+++ src/main/java/com/driver/B.java
@@ -0,0 +1,10 @@
+package com.driver;
+
+public class B extends A {
+
+    @Override
+    public String meth() {
+        String s = "Method is overridden in Extendend class B";
+        return s;
+    }
+}
