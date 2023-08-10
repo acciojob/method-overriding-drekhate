a504e157b1a42b90462bcc34b5d43c135c5103bd
Dipak Rekhate <drekhate@gmail.com>
2023-08-10 15:32:34 +0530

    assi overloding second

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,19 @@
 package com.driver;
 
 public class Main {
+    public static class A {
+        public String meth() {
+            String s = "Invoking method from class A";
+            return s;
+        }
+    }
+    public static class B extends A {
+        @Override
+        public String meth() {
+            String s = "Method is overridden in Extendend class B";
+            return s;
+        }
+    }
     public static void main(String[] args) {
         B obj = new B();
         obj.meth();
